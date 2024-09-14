0fb9a3ccd35cbf87810751b44c8b8e74dce832d6
Aman Mishra <amanmishra@Amans-MacBook-Air.local>
2024-09-14 12:17:15 +0530

    updated

--- cache.py
+++ cache.py
@@ -1,9 +1,11 @@
 import redis
 
 cache = redis.Redis(host='localhost', port=6379, db=0)
-
 def cache_results(user_id, results):
     cache.set(user_id, str(results), ex=3600) 
 
 def get_cached_results(user_id):
-    return cache.get(user_id)
+    cached_data = cache.get(user_id)
+    if cached_data:
+        return eval(cached_data)
+    return None
