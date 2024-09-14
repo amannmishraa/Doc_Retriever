77d8fa389bb14e1678beeeb175c1b85594e6bfc2
Aman Mishra <amanmishra@Amans-MacBook-Air.local>
2024-09-14 11:42:51 +0530

    Final

--- Scraper.py
+++ Scraper.py
@@ -1,3 +1,15 @@
 import threading
 import requests
 from bs4 import BeautifulSoup
+import time
+
+def scrape_news():
+    while True:
+        response = requests.get("https://timesofindia.indiatimes.com/")
+        soup = BeautifulSoup(response.text, "html.parser")
+        articles = soup.find_all("a", class_="storylink")
+        for article in articles[:10]:
+            print(article.text)  
+        time.sleep(3600)  
+
+threading.Thread(target=scrape_news).start()
