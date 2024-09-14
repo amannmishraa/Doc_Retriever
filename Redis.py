f3abc7aa6ec0ce76773291d053af49ac02874388
Aman Mishra <amanmishra@Amans-MacBook-Air.local>
2024-09-14 12:06:33 +0530

    rm

--- Redis.py
+++
@@ -1,9 +0,0 @@
-import redis
-
-cache = redis.Redis(host='localhost', port=6379, db=0)
-
-def cache_results(user_id, results):
-    cache.set(user_id, str(results), ex=3600) 
-
-def get_cached_results(user_id):
-    return cache.get(user_id)
